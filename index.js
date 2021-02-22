0e1cb416725a3c40776991e89b0e069a92edc418
tronghaiit2 <tronghaitoank25cbg@gmail.com>
2021-02-22 17:50:46 +0700

    update all

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var show = document.getElementById("nav-links");
-        function showMenu() {
-            show.style.right ="0";
-        }
-        function closeMenu() {
-            show.style.right ="-120px";
-        }
+    function showMenu() {
+        show.style.right ="0";
+    }
+    function closeMenu() {
+        show.style.right ="-120px";
+    }
 
+var avatar = document.getElementById("avatar");
+var profile = document.getElementById("profile");
+    profile.onclick = function (){
+        if (profile.style.display = "block") {
+            profile.style.display = "none";
+        }
+    }
+    avatar.onclick = function (){
+        if (profile.style.display = "none") {
+            profile.style.display = "block";
+        }
+    }
+    
 
 var circle = document.getElementById("circle");
 var name = document.getElementById("name");  var next = document.getElementById("next");
@@ -17,14 +30,14 @@ var rotateSum;
     next.onclick = function()
     {
         rotateSum = rotateValue + "rotate(-60deg)";
-        home.style.transform = rotateSum;
+        main.style.transform = rotateSum;
         rotateValue = rotateSum;
     }
 
     back.onclick = function()
     {
         rotateSum = rotateValue + "rotate(+60deg)";
-        home.style.transform = rotateSum;
+        main.style.transform = rotateSum;
         rotateValue = rotateSum;
     }
 
